2384767b2ecf06ded8443bd4495ee777baf4bf22
Jwing28 <jonathan.wing.lee@gmail.com>
2016-04-19 15:07:00 -0700

    [create] create user profile page

---
+++ ionic/www/js/controllers/userProfileController.js
@@ -0,0 +1,20 @@
+(function() {
+
+var module = angular.module('wingme.controllers');
+
+module.controller('ProfileCtrl', function($http, $scope, $state) {
+  
+  var vm = this;
+
+  vm.postProfile = function() {
+    var userProfile = {
+
+    };
+  }
+
+  // vm.clearFields = function () {
+    
+  // }
+});
+
+})();
