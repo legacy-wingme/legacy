ed4e53be54059d093d852aa6bcc0398a989fd4ef
bash (wget -qO- https://raw.githubusercontent.com/ga-instructors/installfest_script/master/builds/ubuntu) bash (wget -qO- https://raw.githubusercontent.com/ga-instructors/installfest_script/master/builds/ubuntu) <jacklee0223@gmail.com>
2016-04-20 20:20:06 -0700

    [fix] fix merge conflict

--- ionic/www/js/controllers/addWingController.js
+++ ionic/www/js/controllers/addWingController.js
@@ -3,15 +3,14 @@
 var module = angular.module('wingme.controllers');
 
 module.controller('AddWingCtrl', function(Wings, Sms) {
-
-	var vm = this;
-	vm.submit = function(username){
-		Wings.addWingPost(username);
-    Sms.postsms().then(function($state){
-    	$state.go($state.current, {}, {reload: true, inherit: false});
-    })
-	};
-
+  var vm = this;
+  vm.submit = function(username){
+    console.log('text sent');
+    Wings.addWingPost(username);
+    // Sms.postsms().then(function($state){
+    //   $state.go($state.current, {}, {reload: true, inherit: false});
+    // })
+  };
 
 });
 
