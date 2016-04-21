4287fb1be3f152ee3b68e5805a8b0ad69904b926
bash (wget -qO- https://raw.githubusercontent.com/ga-instructors/installfest_script/master/builds/ubuntu) bash (wget -qO- https://raw.githubusercontent.com/ga-instructors/installfest_script/master/builds/ubuntu) <jacklee0223@gmail.com>
2016-04-20 20:11:34 -0700

    [feat] change sid

--- ionic/www/js/controllers/addWingController.js
+++ ionic/www/js/controllers/addWingController.js
@@ -2,12 +2,15 @@
 
 var module = angular.module('wingme.controllers');
 
-module.controller('AddWingCtrl', function(Wings) {
-	var vm = this;
-	vm.submit = function(username){
-		Wings.addWingPost(username);
-		$state.go($state.current, {}, {reload: true, inherit: false});
-	};
+module.controller('AddWingCtrl', function(Wings, Sms) {
+  var vm = this;
+  vm.submit = function(username){
+    console.log('text sent');
+    Wings.addWingPost(username);
+    // Sms.postsms().then(function($state){
+    //   $state.go($state.current, {}, {reload: true, inherit: false});
+    // })
+  };
 
 });
 
